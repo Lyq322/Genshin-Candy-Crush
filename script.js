545e98ed38374c02f7864c94480502b31a06115b
Yuqing Liu <47932418+Lyq322@users.noreply.github.com>
2022-02-19 04:43:41 +0000

    Added animations for moving and icons for character pieces

--- script.js
+++ script.js
@@ -10,24 +10,62 @@ var row = 6
 
 var imageW = 40
 var imageH = 40
-var padding = 3
-var offsetLeft = 75
-var offsetUp = 75
+var padding = 5
 
+var gridOffsetLeft = 75
+var gridOffsetUp = 75
+
+// Top offset for each piece
+// Used for moving animations
+pieceOffsetUp = new Array(col)
+for (let c = 0; c < col; c++) {
+  pieceOffsetUp[c] = new Array(row)
+  for (let r = 0; r < row; r++) {
+    pieceOffsetUp[c][r] = 0
+  }
+}
+
+// Images for the game pieces
+var folder = "images/"
 var pyroImage = new Image()
-pyroImage.src = "pyro.png"
+pyroImage.src = folder + "pyro.png"
 var cryoImage = new Image()
-cryoImage.src = "cryo.png"
+cryoImage.src = folder + "cryo.png"
 var hydroImage = new Image()
-hydroImage.src = "hydro.png"
+hydroImage.src = folder + "hydro.png"
 var electroImage = new Image()
-electroImage.src = "electro.png"
+electroImage.src = folder + "electro.png"
 var anemoImage = new Image()
-anemoImage.src = "anemo.png"
+anemoImage.src = folder + "anemo.png"
 var geoImage = new Image()
-geoImage.src = "geo.png"
+geoImage.src = folder + "geo.png"
 
-images = [pyroImage, cryoImage, hydroImage, electroImage, anemoImage, geoImage]
+var hutaoImage = new Image()
+hutaoImage.src = folder + "Hutao.png"
+var ayakaImage = new Image()
+ayakaImage.src = folder + "Ayaka.png"
+var kokomiImage = new Image()
+kokomiImage.src = folder + "Kokomi.png"
+var ittoImage = new Image()
+ittoImage.src = folder + "Itto.png"
+var raidenImage = new Image()
+raidenImage.src = folder + "Raiden.png"
+var kazuhaImage = new Image()
+kazuhaImage.src = folder + "Kazuha.png"
+var yoimiyaImage = new Image()
+yoimiyaImage.src = folder + "Yoimiya.png"
+var childeImage = new Image()
+childeImage.src = folder + "Childe.png"
+var albedoImage = new Image()
+albedoImage.src = folder + "Albedo.png"
+var yaeImage = new Image()
+yaeImage.src = folder + "Yae.png"
+var ganyuImage = new Image()
+ganyuImage.src = folder + "Ganyu.png"
+var ventiImage = new Image()
+ventiImage.src = folder + "Venti.png"
+
+images = [pyroImage, cryoImage, hydroImage, electroImage, anemoImage, geoImage, hutaoImage, ayakaImage, kokomiImage, ittoImage, raidenImage, kazuhaImage, yoimiyaImage, childeImage, albedoImage, yaeImage, ganyuImage, ventiImage]
 
 
 var board = new Array(col)
@@ -41,12 +79,15 @@ for (let c = 0; c < col; c++) {
 var clickedPieceC, clickedPieceR
 var click = 0
 
+var sleepTime = 20
+
 // Wait until all images loaded
 counter = 0
-for (let i = 0; i < 6; i++) {
+for (let i = 0; i < 12; i++) {
   images[i].onload = function () {
     counter++
-    if (counter == 6) {
+    if (counter == 12) {
+      console.log("all loaded")
       initialDraw()
     }
   }
@@ -54,41 +95,56 @@ for (let i = 0; i < 6; i++) {
 
 function initialDraw() {
   updateBoard(board)
-  ctx.strokeRect(offsetLeft - 10, offsetUp - 10, col * imageW + (col - 1) * padding + 20, row * imageH + (row - 1) * padding + 20)
+  ctx.strokeRect(gridOffsetLeft - 10, gridOffsetUp - 10, col * imageW + (col - 1) * padding + 20, row * imageH + (row - 1) * padding + 20)
   document.addEventListener("click", clicked, false);
   moveFunc()
 }
 
 function clicked(e) {
+  // Get coordinates of click
   const rect = canvas.getBoundingClientRect()
   const clickX = e.clientX - rect.left
   const clickY = e.clientY - rect.top
+
   for (let c = 0; c < col; c++) {
     for (let r = 0; r < row; r++) {
-      var pieceTop = offsetUp + imageH * r + padding * r
-      var pieceBottom = offsetUp + imageH * (r + 1) + padding * r
-      var pieceLeft = offsetLeft + imageW * c + padding * c
-      var pieceRight = offsetLeft + imageW * (c + 1) + padding * c
+      // Calculate boundaries of each piece
+      var pieceTop = gridOffsetUp + imageH * r + padding * r
+      var pieceBottom = gridOffsetUp + imageH * (r + 1) + padding * r
+      var pieceLeft = gridOffsetLeft + imageW * c + padding * c
+      var pieceRight = gridOffsetLeft + imageW * (c + 1) + padding * c
+
+      // Check if click lands on piece
       if (pieceLeft <= clickX && clickX <= pieceRight && pieceTop <= clickY && clickY <= pieceBottom) {
         click++
+        // First click
         if (click == 1) {
           clickedPieceC = c
           clickedPieceR = r
+
           highlight(c, r)
         }
+        // Second click
         else {
+          // Check if piece is valid
           if ((c == clickedPieceC && r == clickedPieceR + 1) || (c == clickedPieceC && r == clickedPieceR - 1) || (c == clickedPieceC + 1 && r == clickedPieceR) || (c == clickedPieceC - 1 && r == clickedPieceR)) {
             swap(c, r, clickedPieceC, clickedPieceR)
             updateBoard()
+
+            // Check for consecutives, remove them, and move pieces down
             var consecutive = moveFunc()
+            
+            // Don't swap if same click or click not valid
             if (!consecutive) {
               swap(c, r, clickedPieceC, clickedPieceR)
               updateBoard()
             }
           }
+          // Erase highlight if same piece clicked
           else if (c == clickedPieceC && r == clickedPieceR) {
             updateBoard()
           }
+          // Reset click
           click = 0
           updateBoard()
         }
@@ -98,19 +154,21 @@ function clicked(e) {
 }
 
 function updateBoard() {
+  // Clear board
   ctx.clearRect(0, 0, 400, 400)
   for (let c = 0; c < col; c++) {
     for (let r = 0; r < row; r++) {
       if (board[c][r] != -1) {
         ctx.beginPath()
-        ctx.drawImage(images[board[c][r]], offsetLeft + imageW * c + padding * c, offsetUp + imageH * r + padding * r, imageW, imageH)
+        ctx.drawImage(images[board[c][r]], gridOffsetLeft + imageW * c + padding * c, gridOffsetUp + pieceOffsetUp[c][r] + imageH * r + padding * r, imageW, imageH)
         ctx.stroke()
         ctx.closePath()
       }
     }
   }
+  // Frame
   ctx.lineWidth = 10;
-  ctx.strokeRect(offsetLeft - 10, offsetUp - 10, col * imageW + (col - 1) * padding + 20, row * imageH + (row - 1) * padding + 20)
+  ctx.strokeRect(gridOffsetLeft - 10, gridOffsetUp - 10, col * imageW + (col - 1) * padding + 20, row * imageH + (row - 1) * padding + 20)
 }
 
 function swap(c, r, clickedPieceC, clickedPieceR) {
@@ -119,8 +177,9 @@ function swap(c, r, clickedPieceC, clickedPieceR) {
   board[clickedPieceC][clickedPieceR] = temp
 }
 
-
 function checkConsecutive() {
+  // Temp array to store all the pieces that need to be deleted
+  // First find every piece that need to be deleted, then delete everything at the same time to avoid problems
   var temp = new Array(col);
   for (let c = 0; c < col; c++) {
     temp[c] = new Array(row);
@@ -128,10 +187,11 @@ function checkConsecutive() {
       temp[c][r] = 0;
     }
   }
+
   var consecutive = false
   for (let c = 0; c < col; c++) {
     for (let r = 0; r < row; r++) {
-      // up down
+      // Consecutives up down
       if (r + 2 < row && board[c][r] != -1 && board[c][r] == board[c][r + 1] && board[c][r + 1] == board[c][r + 2]) {
         if (r + 3 < row && board[c][r] == board[c][r + 3]) {
           if (r + 4 < row && board[c][r] == board[c][r + 4]) {
@@ -144,7 +204,7 @@ function checkConsecutive() {
         temp[c][r] = -1
         consecutive = true
       }
-      // left pieceRight
+      // Consecutives left right
       if (c + 2 < col && board[c][r] != -1 && board[c][r] == board[c + 1][r] && board[c + 1][r] == board[c + 2][r]) {
         if (c + 3 < col && board[c][r] == board[c + 3][r]) {
           if (c + 4 < col && board[c][r] == board[c + 4][r]) {
@@ -159,6 +219,8 @@ function checkConsecutive() {
       }
     }
   }
+
+  // Delete consecutive pieces
   for (let c = 0; c < col; c++) {
     for (let r = 0; r < row; r++) {
       if (temp[c][r] == -1) {
@@ -169,20 +231,36 @@ function checkConsecutive() {
   return consecutive
 }
 
+// Async function to use await
 async function moveFunc() {
   var consecutive = false
+  // Checks for chain reactions
   while (checkConsecutive()) {
     consecutive = true
+    // Move down one row every 500 milliseconds
     while (checkMove2()) {
-      await sleep(500)
+      for (let i = 0; i < 8; i++) {
+        await sleep(sleepTime)
+        //moveDownOne()
+        moveAnimation()
+        updateBoard()
+      }
+      for (let c = 0; c < col; c++) {
+        for (let r = 0; r < row; r++) {
+          pieceOffsetUp[c][r] = 0
+        }
+      }
+      await sleep(sleepTime)
       moveDownOne()
       updateBoard()
     }
-    await sleep(500)
+    await sleep(sleepTime)
   }
   return consecutive
 }
 
+// Not using anymore
+// Using moveDownOne instead
 function moveDown() {
   for (let c = 0; c < col; c++) {
     var index = 0
@@ -202,11 +280,13 @@ function moveDown() {
   }
 }
 
+// Moves down by once
 function moveDownOne() {
   for (let c = 0; c < col; c++) {
     var empty = false
     var temp = new Array(row)
     var index = row - 1
+    // Delete one empty element (-1) from each empty range
     for (let r = row - 1; r > -1; r--) {
       if (!empty && board[c][r] == -1) {
         empty = true
@@ -231,12 +311,33 @@ function moveDownOne() {
     for (let r = row - 1; r > index; r--) {
       board[c][r] = temp[r]
     }
+
+    // Add random piece to the top
     if (board[c][0] == -1) {
       board[c][0] = Math.floor(6 * Math.random())
     }
   }
 }
 
+function moveAnimation() {
+  for (let c = 0; c < col; c++) {
+    var index = -1
+    for (let r = row - 1; r > -1; r--) {
+      if (board[c][r] == -1) {
+        index = r
+        break
+      }
+    }
+    for (let r = index; r > -1; r--) {
+      if (board[c][r] != -1) {
+        pieceOffsetUp[c][r] += 5
+      }
+    }
+  }
+}
+
+// Not using anymore
+// Using checkMove2 instead
 function checkMove1() {
   var needMove = false
   for (let c = 0; c < col; c++) {
@@ -255,6 +356,7 @@ function checkMove1() {
   return needMove
 }
 
+// Checks if still need to move pieces
 function checkMove2() {
   for (let c = 0; c < col; c++) {
     for (let r = 0; r < row; r++) {
@@ -266,6 +368,8 @@ function checkMove2() {
   return false
 }
 
+// Not using anymore
+// Using an iterative method instead
 function moveRecursive() {
   if (checkMove2()) {
     setTimeout(function() {
@@ -276,11 +380,14 @@ function moveRecursive() {
   }
 }
 
+// Draws a box around the piece clicked to highlight it
 function highlight(pieceC, pieceR) {
   ctx.lineWidth = 2
-  ctx.strokeRect(offsetLeft + imageW * pieceC + padding * pieceC, offsetUp + imageH * pieceR + padding * pieceR, imageW, imageH)
+  ctx.strokeRect(gridOffsetLeft + imageW * pieceC + padding * pieceC, gridOffsetUp + imageH * pieceR + padding * pieceR, imageW, imageH)
 }
 
+// Not using anymore
+// Only used for puzzle version
 function checkWin(board) {
   var empty = true
   for (let c = 0; c < col; c++) {
@@ -303,6 +410,7 @@ function checkWin(board) {
 }
 
 // https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep/39914235#39914235
+// Alternative to setTimeout
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
